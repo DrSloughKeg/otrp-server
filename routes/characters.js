54dab1c620a83fe6283632c1588c5619ad0e61d7
DrSloughKeg <bpye1995@gmail.com>
2024-02-29 19:44:29 -0500

    post create character

--- routes/characters.js
+++ routes/characters.js
@@ -1,16 +1,39 @@
 const express = require("express");
 const router = express.Router();
 const { characters } = require("../models");
+const { users } = require("../models");
 const { validateToken } = require("../middleware/authmiddleware");
 
-router.post("/create", async (req, res) => {
-  const {} = req.body;
+router.post("/create", validateToken, async (req, res) => {
+  //calculate HP and AP
+  var hp = 10;
+  var ap = 10;
+  if (req.body.charClass == "warrior") {
+    hp += 2;
+    ap -= 4;
+  } else if (req.body.charClass == "wizard") {
+    hp -= 2;
+    ap += 4;
+  }
+  hp += (req.body.con - 10) / 2;
+
+  const userId = req.userToken.id;
   try {
-    //const user = await users.findOne({ where: { username: username } });
+    const user = await users.findOne({ where: { userId: userId } });
     if (!user) {
     } else {
-      res.json({ error: "" });
+      res.json({ error: "user not logged in" });
     }
+    await characters.create({
+      userId: userId,
+      hp: hp,
+      lvl: 1,
+      charName: req.body.name,
+      ap: ap,
+      evnt: 0,
+      class: req.body.charClass,
+      ...req.body,
+    });
   } catch (error) {
     console.log(error);
     res.json({ error: "DB connection issue?" });
